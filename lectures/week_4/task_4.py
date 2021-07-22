7a8923a80cd75328338dfa72d29e51ebdf898be5
Zaiyr <zaiyr.00@gmail.com>
2021-07-22 22:40:59 +0600

    Week 4

--- lectures/week_4/task_4.py
+++ lectures/week_4/task_4.py
@@ -1,12 +1,65 @@
-# Словарь. Задел под оптимизацию.
+# 3. Задел под оптимизацию.
 
+# Задача
 # Сгруппировать слова по общим буквам
 # Sample Input: ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
-# Sample Output: [['eat', 'tea', 'ate'], ['nat', 'tan'], ['bat']]
+# Sample Output: [['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat']]
+
+# Решение здорового человека
+# Отсортируем в каждом слове буквы и это будет выступать в роли 
+# ключа, а значение будет список слов
+
+def group_words(words):
+    groups = {}
+    for word in words:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word not in groups:
+            groups[sorted_word] = []
+        groups[sorted_word].append(word)
+    ans = []
+    for sorted_word in groups:
+        ans.append(groups[sorted_word])
+    return ans
+
+print(group_words(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+
+# ------------------------------------------------------------------
+
+# Подозрения здорового человека
+# sortedword = ''.join(sorted(word))
+# Вдруг слово будет длинное (N)? Сортировка займет O(N logN). 
+# Количество различных букв в слове K <= N, можем посчитать количество 
+# каждой за O(N) и отсортировать за O(K logK), теоритически
+
+# ------------------------------------------------------------------
+
+# Задел здорового человека
+# Будет тормозить - посмотрим на профилировщике где, и если долго 
+# считается ключ - легко поправим на что-то более эффективное
 
 def group_words(words):
     def key_by_word(word):
-        # return ''.join(sorted(word))
+        return ''.join(sorted(word))
+            
+    groups = {}
+    for word in words:
+        sorted_word = key_by_word(word)
+        if sorted_word not in groups:
+            groups[sorted_word] = []
+        groups[sorted_word].append(word)
+    ans = []
+    for sorted_word in groups:
+        ans.append(groups[sorted_word])
+    return ans
+
+print(group_words(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+
+# ------------------------------------------------------------------
+
+# Оптимизация (?) здорового человека
+
+def group_words(words):
+    def key_by_word(word):
         syms = {}
         for sym in word:
             if sym not in syms:
